d31cd95b6172d2c9eec357a4e7fd1cdb6b0b402c
Casper J <casper.jaller@gmail.com>
2021-03-17 02:39:42 +0100

    Update 2021-03-17#02:39 - manifest.json

--- src/components/About.js
+++ src/components/About.js
@@ -3,6 +3,7 @@ import {Link} from 'react-router-dom'
 const About = () => {
     return (
         <div>
+            <h2>Much react, such doge!</h2>
             <h4>version 0.0.2</h4>
             <Link to='/'>Back</Link>
         </div>
